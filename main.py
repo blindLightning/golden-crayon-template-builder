5493bff11f70b415ec603012adfa0b4d15cde2d4
blindLightning <77203463+blindLightning@users.noreply.github.com>
2021-05-08 20:46:00 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+#the golden crayon template builder
+import lucia
+from time import sleep
+#pyperclip will just allow the rawdata to be copied to clipboard if you've not heard of the module, not sure how well known it is, lol
+from pyperclip import copy
+if not lucia.running: lucia.initialize()
+
+
